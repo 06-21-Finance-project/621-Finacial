f974aedb1ba4f8534bbbed4322289bb876a8c1c2
Pattarin <pattarin.won@ku.th>
2021-11-25 00:37:43 +0700

    fetch stocks

--- finance_app/views.py
+++ finance_app/views.py
@@ -1,7 +1,57 @@
-from django.http import HttpResponse
+import environ
+from django.shortcuts import render
+import requests
 
+env = environ.Env()
+environ.Env.read_env()
+
+API_KEY = env('API_TOKEN')
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    context = fetch_news(request)
+    return render(request, 'index.html', context)
 
-# Create your views here.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def fetch_news(request):
+    url = 'https://api.stockdata.org/v1/data/quote'
+    stocks_symbol = ["IQV,ENB,FB", "AMZN,BKNG,TSLA", "AAPL,GOOGL,MSFT", "MA"]
+    context = {}
+    stock_details = []
+    for i in range(4):
+        querystring = {"api_token": API_KEY, "symbols": stocks_symbol[i]}
+        response = requests.request("GET", url, params=querystring)
+        response = response.json()
+        stock_details = stock_details + response["data"]
+    context["stock_data"] = stock_details
+    return context
+
+
